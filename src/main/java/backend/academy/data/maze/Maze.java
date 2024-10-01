4379829acf14be3ee26029cc9f3ff05f08b16289
Roman Ryabikin <riabikin.roman@gmail.com>
2024-10-01 16:43:09 +0300

    feat: add utils methods for Maze DTO classes

--- src/main/java/backend/academy/data/maze/Maze.java
+++ src/main/java/backend/academy/data/maze/Maze.java
@@ -1,13 +1,53 @@
 package backend.academy.data.maze;
 
+import static backend.academy.data.maze.CellType.WALL;
+
 /**
  * Class representing the maze
+ *
  * @param height height of the maze
  * @param width  width of the maze
  * @param grid   2D array of cells representing the maze
  */
 public record Maze(int height, int width, Cell[][] grid) {
     public Maze(int height, int width) {
-        this(height, width, new Cell[height][width]);
+        this(height, width, initGrid(height, width));
+    }
+
+    /**
+     * Initialize the grid with walls
+     *
+     * @param height height of the maze
+     * @param width  width of the maze
+     * @return 2D array of cells representing the maze
+     */
+    private static Cell[][] initGrid(int height, int width) {
+        var maze = new Cell[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                maze[i][j] = new Cell(Point.of(i, j), WALL);
+            }
+        }
+        return maze;
+    }
+
+    /**
+     * Get the cell at the given point
+     *
+     * @param point the point to get the cell from
+     * @return the cell at the given point
+     */
+    public Cell getCell(Point point) {
+        return grid[point.row()][point.col()];
+    }
+
+    /**
+     * Set the cell at the given point
+     *
+     * @param point the point to set the cell at
+     * @param type  the type of the cell
+     */
+    public void setPoint(Point point, CellType type) {
+        grid[point.row()][point.col()] = new Cell(point, type);
     }
 }
--- src/main/java/backend/academy/data/maze/Point.java
+++ src/main/java/backend/academy/data/maze/Point.java
@@ -1,9 +1,83 @@
 package backend.academy.data.maze;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Class representing the point coordinates
- * @param row
- * @param col
  */
 public record Point(int row, int col) {
+    public static Point of(int row, int col) {
+        return new Point(row, col);
+    }
+
+    /**
+     * Returns the upper neighbour of the point
+     *
+     * @return the upper neighbour or null if the point is on the top row
+     */
+    public Point upper() {
+        if (row == 0) {
+            return null;
+        }
+        return new Point(row - 1, col);
+    }
+
+    /**
+     * Returns the lower neighbour of the point
+     *
+     * @param maxHeight the maximum height of the maze
+     * @return the lower neighbour or null if the point is on the bottom row
+     */
+    public Point lower(int maxHeight) {
+        if (row == maxHeight - 1) {
+            return null;
+        }
+        return new Point(row + 1, col);
+    }
+
+    /**
+     * Returns the left neighbour of the point
+     *
+     * @return the left neighbour or null if the point is on the leftmost column
+     */
+    public Point left() {
+        if (col == 0) {
+            return null;
+        }
+        return new Point(row, col - 1);
+    }
+
+    /**
+     * Returns the right neighbour of the point
+     *
+     * @param maxWidth the maximum width of the maze
+     * @return the right neighbour or null if the point is on the rightmost column
+     */
+    public Point right(int maxWidth) {
+        if (col == maxWidth - 1) {
+            return null;
+        }
+        return new Point(row, col + 1);
+    }
+
+    /**
+     * Returns a list of all neighbours of the point
+     *
+     * @param maze the maze
+     * @return the list of neighbours
+     */
+    public List<Point> getNeighbours(Maze maze) {
+        List<Point> neighbours = new ArrayList<>();
+        Optional.ofNullable(this.upper())
+            .ifPresent(neighbours::add);
+        Optional.ofNullable(this.lower(maze.height()))
+            .ifPresent(neighbours::add);
+        Optional.ofNullable(this.left())
+            .ifPresent(neighbours::add);
+        Optional.ofNullable(this.right(maze.width()))
+            .ifPresent(neighbours::add);
+        return neighbours;
+    }
 }
